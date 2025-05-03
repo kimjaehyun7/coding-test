6b1a2c3f201e142f2cd80b54af4770645b07a7ee
jaehyun <wogusrla77@naver.com>
2025-05-03 14:12:37 +0900

    5338

---
+++ src/baekjoon/Baekjoon_5338.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.io.*;
+
+public class Baekjoon_5338 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(
+                "       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|"
+        );
+
+        bw.flush();
+        bw.close();
+    }
+}
