df248f177f65217c619728e27e980cb845e9b8ca
jaehyun <wogusrla77@naver.com>
2025-05-04 19:12:48 +0900

    2522

---
+++ src/baekjoon/Baekjoon_2522.java
@@ -0,0 +1,47 @@
+package baekjoon;
+
+import java.io.*;
+
+public class Baekjoon_2522 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int star = 1;
+        int empty = n - 1;
+        boolean change = false;
+
+        for (int i = 0; i < 2 * n - 1; i++) {
+            if (star == n) {
+                change = true;
+            }
+            if (!change) {
+                for (int j = 0; j < empty; j++) {
+                    bw.write(" ");
+                }
+                for (int j = 0; j < star; j++) {
+                    bw.write("*");
+                }
+                bw.write("\n");
+                star++;
+                empty--;
+            } else {
+                for (int j = 0; j < empty; j++) {
+                    bw.write(" ");
+                }
+                for (int j = 0; j < star; j++) {
+                    bw.write("*");
+                }
+                bw.write("\n");
+                star--;
+                empty++;
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
