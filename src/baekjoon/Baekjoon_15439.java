bda26950ffe893c978fe9f423fa61a8e99322169
jaehyun <wogusrla77@naver.com>
2025-05-20 13:10:46 +0900

    15439

--- src/baekjoon/Baekjoon_15439.java
+++ src/baekjoon/Baekjoon_15439.java
@@ -2,18 +2,16 @@ package baekjoon;
 
 import java.io.*;
 
-public class Baekjoon_1834 {
+public class Baekjoon_15439 {
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        long n = Long.parseLong(br.readLine());
-        long sum = (n + 1) * (n - 1) * n / 2;
-        bw.write(sum+"");
+        int n = Integer.parseInt(br.readLine());
 
-
+        bw.write((n - 1) * n + "");
         bw.flush();
         bw.close();
         br.close();
