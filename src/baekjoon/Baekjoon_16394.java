ed64a3c5228fbfa0409fd2a1a733df349246db65
jaehyun <wogusrla77@naver.com>
2025-06-04 14:25:00 +0900

    16394

---
+++ src/baekjoon/Baekjoon_16394.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Baekjoon_16394 {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        System.out.println(input - 1946);
+    }
+}
