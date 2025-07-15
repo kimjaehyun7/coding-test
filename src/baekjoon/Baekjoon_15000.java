1cb48794eb6d3627dc39e9bb89345bb103674f14
jaehyun <wogusrla77@naver.com>
2025-07-15 11:03:50 +0900

    15000

---
+++ src/baekjoon/Baekjoon_15000.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.io.*;
+
+public class Baekjoon_15000 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(br.readLine().toUpperCase());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
