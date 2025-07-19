41040a85a79271bcce02d9518865f58175a6a488
jaehyun <wogusrla77@naver.com>
2025-07-19 11:12:23 +0900

    23234

---
+++ src/baekjoon/Baekjoon_23234.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.io.*;
+
+public class Baekjoon_23234 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("The world says hello!");
+        bw.flush();
+        bw.close();
+
+    }
+}
