75684c14b532ae1030da7e2c83e72780bccabc02
jaehyun <wogusrla77@naver.com>
2025-08-05 09:50:13 +0900

    14935

---
+++ src/baekjoon/Baekjoon_14935.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.ArrayDeque;
+
+public class Baekjoon_14935 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        br.readLine();
+
+        bw.write("FA");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
