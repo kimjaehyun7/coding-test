5847ecc8b4d1de39b377b440caeb370e98c160e4
jaehyun <wogusrla77@naver.com>
2025-08-09 12:27:28 +0900

    6778

---
+++ src/baekjoon/Baekjoon_6778.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.io.*;
+
+public class Baekjoon_6778 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int antenna = Integer.parseInt(br.readLine());
+        int eyes = Integer.parseInt(br.readLine());
+
+        if (antenna >= 3 && eyes <= 4) {
+            bw.write("TroyMartian\n");
+        }
+        if (antenna <= 6 && eyes >= 2) {
+            bw.write("VladSaturnian\n");
+        }
+        if (antenna <= 2 && eyes <= 3) {
+            bw.write("GraemeMercurian");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
