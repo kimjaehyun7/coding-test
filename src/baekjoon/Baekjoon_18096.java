9b30c159bca54243ae4a4f83cf4b6a0ca8006c3c
jaehyun <wogusrla77@naver.com>
2025-08-10 11:38:56 +0900

    18096

---
+++ src/baekjoon/Baekjoon_18096.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.io.*;
+
+public class Baekjoon_18096 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        br.readLine();
+
+
+        bw.write("1");
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
