e1976f3a3dd23bff1ac8df12b940075ddb81caac
jaehyun <wogusrla77@naver.com>
2025-09-08 10:14:59 +0900

    18409

---
+++ src/baekjoon/Baekjoon_18409.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.io.*;
+
+public class Baekjoon_18409 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        int count = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                count++;
+            }
+        }
+
+        bw.write(count + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
