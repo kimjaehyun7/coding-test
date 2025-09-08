3017af0a575a5e7669c10260981d38e16ba49d75
jaehyun <wogusrla77@naver.com>
2025-09-09 08:34:25 +0900

    30328

---
+++ src/baekjoon/Baekjoon_30328.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.io.*;
+
+public class Baekjoon_30328 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        bw.write(n * 4000 + "");
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
