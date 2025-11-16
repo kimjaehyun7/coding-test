adcf0b5ec98c89d188bac67baa1c7de3515b4433
jaehyun <wogusrla77@naver.com>
2025-11-16 13:39:16 +0900

    27324

---
+++ src/baekjoon/Baekjoon_27324.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.io.*;
+
+public class Baekjoon_27324 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        char[] arr = br.readLine().toCharArray();
+
+        if (arr[0] == arr[1]) {
+            bw.write("1");
+        } else {
+            bw.write("0");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
