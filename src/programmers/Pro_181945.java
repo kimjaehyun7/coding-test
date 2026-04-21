59803af18887826852e2a950317e4c027730c51f
jaehyun <wogusrla77@naver.com>
2026-04-21 11:59:21 +0900

    pro_181945

---
+++ src/programmers/Pro_181945.java
@@ -0,0 +1,14 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class Pro_181945 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        for (String s : a.split("")) {
+            System.out.println(s);
+        }
+    }
+}
