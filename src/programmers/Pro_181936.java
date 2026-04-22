8c4337905091095f3d4f5bf4e63d63bbf0d273db
jaehyun <wogusrla77@naver.com>
2026-04-22 13:44:01 +0900

    pro_181936

---
+++ src/programmers/Pro_181936.java
@@ -0,0 +1,8 @@
+package programmers;
+
+public class Pro_181936 {
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+        return answer = (number % n == 0 && number % m == 0) ? 1 : 0;
+    }
+}
