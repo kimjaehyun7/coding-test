b7acccdeb04b5f2e0a85b3c5e425bf8e1de89cdd
jaehyun <wogusrla77@naver.com>
2026-04-22 13:34:02 +0900

    pro_181940

---
+++ src/programmers/Pro_181940.java
@@ -0,0 +1,14 @@
+package programmers;
+
+public class Pro_181940 {
+    public String solution(String my_string, int k) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < k; i++) {
+            sb.append(my_string);
+        }
+
+        return answer = sb.toString();
+    }
+}
