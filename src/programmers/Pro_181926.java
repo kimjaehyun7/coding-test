17f24320602f8510993c85724c13cff14c9cb9ab
jaehyun <wogusrla77@naver.com>
2026-04-23 12:10:47 +0900

    pro_181926

---
+++ src/programmers/Pro_181926.java
@@ -0,0 +1,25 @@
+package programmers;
+
+public class Pro_181926 {
+    public int solution(int n, String control) {
+        int answer = n;
+
+        for (char c : control.toCharArray()) {
+            switch (c) {
+                case 'w':
+                    answer++;
+                    break;
+                case 's':
+                    answer--;
+                    break;
+                case 'd':
+                    answer += 10;
+                    break;
+                case 'a':
+                    answer -= 10;
+                    break;
+            }
+        }
+        return answer;
+    }
+}
