5656674a52e763addad6fc38d01de6927c5eec90
jaehyun <wogusrla77@naver.com>
2026-04-23 12:20:25 +0900

    pro_181924

---
+++ src/programmers/Pro_181924.java
@@ -0,0 +1,18 @@
+package programmers;
+
+public class Pro_181924 {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+
+        for (int i = 0; i < queries.length; i++) {
+            int j = queries[i][0];
+            int k = queries[i][1];
+
+            int temp = arr[j];
+            arr[j] = arr[k];
+            arr[k] = temp;
+        }
+
+        return answer = arr;
+    }
+}
