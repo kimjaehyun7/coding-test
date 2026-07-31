a8844c9bb535deadf0b355713973b3233fb60732
jaehyun <wogusrla77@naver.com>
2026-07-31 14:23:19 +0900

    code_630A

---
+++ src/codeforces/Code_630A.java
@@ -0,0 +1,15 @@
+package codeforces;
+
+import java.io.*;
+
+public class Code_630A {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long n = Long.parseLong(br.readLine());
+
+        bw.write("25");
+        bw.flush();
+    }
+}
